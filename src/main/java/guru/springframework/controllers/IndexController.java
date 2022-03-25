6be3331462ef8fb623cdbf4fb48ac4c96af49795
rishabhsvats <rishabhsvats@gmail.com>
2022-03-25 18:53:08 +0530

    added projectlombok and refactored classes according to lombok

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -2,9 +2,10 @@ package guru.springframework.controllers;
 
 import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,24 +13,23 @@ import java.util.Optional;
 /**
  * Created by jt on 6/1/17.
  */
+@Slf4j
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    private  final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+    public String getIndexPage(Model model){
 
-        System.out.println("Category Id is: " + categoryOptional.get().getId());
-        System.out.println("Unit of Measure Id is: " + unitOfMeasureOptional.get().getId());
+        log.debug("in index page processing controller");
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
